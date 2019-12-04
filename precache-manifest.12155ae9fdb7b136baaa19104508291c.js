0824c064ae825cc3b59fefa89094209e05c89dc2
theingredients <theingredientscollective@gmail.com>
2019-12-03 20:32:23 -0800

    Updates

---
+++ precache-manifest.12155ae9fdb7b136baaa19104508291c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98b88937b363cd7cb161744bd1f8f0ef",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "cd150175c544e0996e11",
+    "url": "/my-app/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "0f406a5d48aa48db9610",
+    "url": "/my-app/static/js/2.68d38b13.chunk.js"
+  },
+  {
+    "revision": "cd150175c544e0996e11",
+    "url": "/my-app/static/js/main.690a2115.chunk.js"
+  },
+  {
+    "revision": "2067abcc1b1d9bcd4e07",
+    "url": "/my-app/static/js/runtime-main.98b5c931.js"
+  }
+]);
